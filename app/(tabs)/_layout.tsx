c81578c0261d2e18350e1771d53ffbe477cc5b62
chrismazi <119514447+chrismazi@users.noreply.github.com>
2026-01-20 07:45:03 +0200

    chore: update app navigation layout and project configuration

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,54 @@
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { Tabs } from 'expo-router';
+import React from 'react';
+
+import { useClientOnlyValue } from '@/components/useClientOnlyValue';
+import { useColorScheme } from '@/components/useColorScheme';
+import Colors from '@/constants/Colors';
+
+// You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
+function TabBarIcon(props: {
+  name: React.ComponentProps<typeof FontAwesome>['name'];
+  color: string;
+}) {
+  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
+}
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        // Disable the static render of the header on web
+        // to prevent a hydration error in React Navigation v6.
+        headerShown: useClientOnlyValue(false, true),
+      }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Daily',
+          tabBarIcon: ({ color }) => <FontAwesome name="book" size={24} color={color} />,
+          headerTitle: 'Faith App',
+        }}
+      />
+      <Tabs.Screen
+        name="two"
+        options={{
+          title: 'Reflection',
+          tabBarIcon: ({ color }) => <FontAwesome name="pencil" size={24} color={color} />,
+          headerTitle: 'My Reflection',
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: 'Profile',
+          tabBarIcon: ({ color }) => <FontAwesome name="user" size={24} color={color} />,
+          headerTitle: 'Profile',
+        }}
+      />
+    </Tabs>
+  );
+}
